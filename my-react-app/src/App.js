1c7954d29a310d681db71fbc760dfc11231f01d9
Nick <nkostiw@gmail.com>
2024-05-30 08:59:01 -0600

    Initial React app setup

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Web Page</h1>
+          <p>This is my landing page.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
